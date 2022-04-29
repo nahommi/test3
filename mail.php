80a09bc6a1be42d959ef34b9951e1029d34e605d
nahommi <104361441+nahommi@users.noreply.github.com>
2022-04-29 17:08:16 +0300

    Add files via upload

---
+++ mail.php
@@ -0,0 +1,64 @@
+<?php 
+if ( isset($_POST[ 'submit'])) {
+$name = $_post['name'];
+$grade = $_post['grade'];
+$emailfrom = $_post['email'];
+$finalreportcard =$_post['final report card'];
+$Bankreceipt = $_post['Bank receipt'];
+
+$mailto = "nahommitiku5509@gmail.com";
+$headers = "from: ".$emailfrom;
+$txt = "you have received an e-mail from ".$name."\n\n".$message;
+
+mail($mailto, $subject, $txt, $headers);
+header("location: mail.php?mailsend");
+
+$target_dir = "uploads/";
+$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+$uploadOk = 1;
+$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+
+// Check if image file is a actual image or fake image
+if(isset($_POST["submit"])) {
+  $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+  if($check !== false) {
+    echo "File is an image - " . $check["mime"] . ".";
+    $uploadOk = 1;
+  } else {
+    echo "File is not an image.";
+    $uploadOk = 0;
+  }
+}
+
+// Check if file already exists
+if (file_exists($target_file)) {
+  echo "Sorry, file already exists.";
+  $uploadOk = 0;
+}
+
+// Check file size
+if ($_FILES["fileToUpload"]["size"] > 500000) {
+  echo "Sorry, your file is too large.";
+  $uploadOk = 0;
+}
+
+// Allow certain file formats
+if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+&& $imageFileType != "gif" ) {
+  echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+  $uploadOk = 0;
+}
+
+// Check if $uploadOk is set to 0 by an error
+if ($uploadOk == 0) {
+  echo "Sorry, your file was not uploaded.";
+// if everything is ok, try to upload file
+} else {
+  if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+    echo "The file ". htmlspecialchars( basename( $_FILES["fileToUpload"]["name"])). " has been uploaded.";
+  } else {
+    echo "Sorry, there was an error uploading your file.";
+  }
+}
+
+?>
